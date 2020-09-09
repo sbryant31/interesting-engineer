32e59e67525eb217eeade473ac04487397bb2341
Nick Bryant <nick@arcanium.io>
2020-09-08 20:14:30 -0400

    build

---
+++ build/precache-manifest.a4aaf59c2cb6bb75cac4268fa4e2f818.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159087d8faea5bed09e5c7c4adf9dd64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b4f3869e0a610efa879",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7a5ecaea46e0a004e66a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b4f3869e0a610efa879",
+    "url": "/static/js/2.fabf6fb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fabf6fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5ecaea46e0a004e66a",
+    "url": "/static/js/main.6071369b.chunk.js"
+  },
+  {
+    "revision": "615f1fa9447af47ee150",
+    "url": "/static/js/runtime-main.64f5c2fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
